fb1357d70e6f99b4440c21cfd6b085d0ae96b95b
Viktoria <vikamosk@gmail.com>
2022-06-21 15:15:33 +0300

    Change Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@ public class Main {
     public static void main(String[] args) {
 
         // все начальные значения указываются в копейках
-        int startingBalance = 10000;
-        int refillAmount = 110000;
+        int startingBalance = 10_000;
+        int refillAmount = 110_000;
         float finalBalance;
         int bonus = 0;
 
-        if (refillAmount > 100000) {
-            bonus = refillAmount / 10000;
-            finalBalance = (startingBalance + refillAmount) / 100 + bonus;
+        if (refillAmount > 100_000) {
+            bonus = refillAmount / 10_000; // Расчет бонуса в рублях, потому что он целое число без копеек - Сумма пополнения / 100 рублей (10_000 копеек)
+            finalBalance = (startingBalance + refillAmount) / 100 + bonus; // Стартовый баланс и сумма пополнения переводится в рубли + бонус уже в рублях
         } else {
             finalBalance = (startingBalance + refillAmount) / 100;
         }
